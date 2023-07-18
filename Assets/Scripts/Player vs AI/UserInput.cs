c6828c4ac396d2feebc7c011162adc491ce56a1b
andr0human <ayushsinha132001@gmail.com>
2023-07-18 05:54:06 +0530

    Iterative logic for mouseInput.

---
+++ Assets/Scripts/Player vs AI/UserInput.cs
@@ -0,0 +1,100 @@
+
+using UnityEngine;
+using System.Collections;
+
+
+class UserInput : MonoBehaviour
+{
+    public BoardHandler bh;
+
+    public  int InitSquare;
+    public  int DestSquare;
+    public  int PromotedPiece;
+    private int SquareSelected;
+
+
+    private void
+    ShowDestSquaresOnBoard(ref MoveList moves, int square)
+    {
+        bh.BoardReset(true);
+        bh.BoardHighLight(moves.endIndex[square]);
+    }
+
+
+    private bool
+    InsideOfBoard(float __x, float __y)
+    {
+        return (__x >= 0) && (__x < 8)
+            && (__y >= 0) && (__y < 8);
+    }
+
+
+    private IEnumerator
+    MouseClickCoroutine()
+    {
+        int __x = -1, __y = -1;
+        SquareSelected = -1;
+
+        while (true)
+        {
+            yield return null;
+            if (Input.GetMouseButtonDown(0))
+            {
+                // Left mouse button clicked
+
+                // Get the current mouse position
+                Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                __x = (int)(mouse_pos.x + 0.5f);
+                __y = (int)(mouse_pos.y + 0.5f);
+
+                if (InsideOfBoard(__x, __y))
+                    break;
+            }
+        }
+
+        SquareSelected = 8 * __y + __x;
+    }
+
+
+    public void
+    GetSquares(ref MoveList movelist)
+    {
+        InitSquare = -1;
+        DestSquare = -1;
+        StartCoroutine( GetSquaresCoroutine(movelist) );
+    }
+
+
+    private IEnumerator
+    GetSquaresCoroutine(MoveList movelist)
+    {
+        while (true)
+        {
+            StartCoroutine( MouseClickCoroutine() );
+            yield return new WaitUntil(() => SquareSelected != -1);
+
+            if (movelist.ValidInitSquare(SquareSelected))
+            {
+                if (SquareSelected == InitSquare)
+                {
+                    InitSquare = -1;
+                    bh.BoardReset(true);
+                }
+                else
+                {
+                    InitSquare = SquareSelected;
+                    ShowDestSquaresOnBoard(ref movelist, InitSquare);
+                }
+                continue;
+            }
+
+            if ((InitSquare != -1) && movelist.ValidDestSquare(InitSquare, SquareSelected))
+            {
+                DestSquare = SquareSelected;
+                break;
+            }
+        }
+    }
+}
+
